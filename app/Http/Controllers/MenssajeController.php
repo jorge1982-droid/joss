6e3c84ae625b9d0d41ed004577b3ce9dfb3418a4
jorge1982-droid <wako1982133@hotmail.com>
2022-02-21 19:23:34 -0600

    first commit

---
+++ app/Http/Controllers/MenssajeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MenssajeController extends Controller
+{
+   /*  public function index()
+    {
+     $message['user'] = "Juan Perez";
+    $message['message'] =  "Prueba mensaje desde Pusher";
+    $success = event(new App\Events\NewMessage($message));
+    return $success;
+    } */
+}
